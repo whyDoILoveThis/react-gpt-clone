340f127c75596e87498c9a61eaee8784cdb115ae
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2023-10-31 05:42:47 -0400

    Created base frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <section className="side-bar">
+    <button>+ New chat</button>
+      <ul className="history">
+        <li>fdfdfdf</li>
+      </ul>
+      <nav>
+        Made by Ian Thai Smith
+      </nav>
+    </section>
+      <section className="main">
+        <h1>i.t.s. GPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">👉</div>
+          </div>
+          <p className="info">ChatGPT is an AI-powered language model developed by OpenAI, capable of generating human-like text based on context and past conversations.</p>
+        </div>
+      </section>
     </div>
   );
 }
